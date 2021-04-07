8fe66f20b2712eaebfb83c7a294d64c4547579e9
Ferchamin <ferminmorello2@hotmail.com>
2021-04-07 19:31:10 -0300

    Push de las entidades: - Carrito - CarritoItem - Compra

---
+++ CarritoCompras/Models/Carrito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Carrito
+{
+
+	public bool Activo { get; set; }
+	public Cliente Cliente { get; set; }
+	public CarritoItem CarritoItem { get; set; }
+	public double Subtotal { get; set; }
+
+
+	public Carrito()
+	{
+	}
+}
---
+++ CarritoCompras/Models/Compra.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Compra
+{
+
+	public Cliente Cliente { get; set; }
+	public Carrito Carrito { get; set; }
+	public double Total { get; set }
+
+	public Compra()
+	{
+	}
+}
