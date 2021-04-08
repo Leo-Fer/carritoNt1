27889c59f824ff8638294acd6f3cc3db60e0d5d3
Bartoletti Alesis <34713428@comunidadort.edu.ar>
2021-04-08 20:39:37 -0300

    Usuario y Cliente

---
+++ CarritoCompras/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+public class Cliente : Usuario
+{
+    public String dni { get; set; }
+
+    public List<Carrito> carritos { get; set; }
+
+    public List<Compra> compras { get; set; }
+
+
+
+
+
+
+
+    public Cliente()
+	{
+
+        
+
+
+
+	}
+}
---
+++ CarritoCompras/Models/Usuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+ 
+
+public  class Usuario
+{
+
+    public String nombre { get; set; }
+
+    public String direccion { get; set; }
+
+    public String telefono { get; set; }
+
+    public String email { get; set; }
+
+    public String apellido { get; set; }
+
+    public DateTime fechaAlta { get; set; }
+
+    public String password { get; set; }
+
+    public String userRol { get; set; }
+
+
+    public Usuario()
+    {
+
+}
+
+
+
+
+
+
+
+
+
+    
+}
