6ad97a0861ff3c317bdd0dba26f8162c5da60930
Ferchamin <ferminmorello2@hotmail.com>
2021-04-08 13:11:11 -0300

    Lista de CarritoItem agregado

--- CarritoCompras/Models/Carrito.cs
+++ CarritoCompras/Models/Carrito.cs
@@ -5,11 +5,12 @@ public class Carrito
 
 	public bool Activo { get; set; }
 	public Cliente Cliente { get; set; }
-	public CarritoItem CarritoItem { get; set; }
+	public list<CarritoItem> CarritosItem;
 	public double Subtotal { get; set; }
 
 
 	public Carrito()
 	{
+		CarritosItem = new list<CarritoItem>();
 	}
 }
