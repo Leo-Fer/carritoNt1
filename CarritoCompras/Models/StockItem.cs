0c7d3490904529582a059a8e41d5f2aec37e2c31
Leo Fernandez <31252634@comunidadort.edu.ar>
2021-04-08 17:15:03 -0300

    Creacion de entidades StockItem y Sucursal. Declaracion de sus propiedades.

---
+++ CarritoCompras/Models/StockItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class StockItem
+{
+	public Sucursal Sucursal { get; set }
+	public Producto Producto { get; set }
+	public int Cantidad { get; set }
+
+	public StockItem()
+	{
+	}
+}
---
+++ CarritoCompras/Models/Sucursal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Sucursal
+{
+
+	public string Nombre { get; set }
+	public string Direccion { get; set }
+	public string Telefono { get; set }
+	public string Email { get; set }
+
+	public List<StockItem> Stockitems { get; set }
+
+	public Sucursal()
+	{
+	}
+}
