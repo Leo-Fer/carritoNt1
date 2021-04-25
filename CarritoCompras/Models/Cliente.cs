ba731bfa58ecbabd9b29a377ef4165b48590a8be
Leo Fernandez <31252634@comunidadort.edu.ar>
2021-04-25 14:24:21 -0300

    Arreglado relaciones y propiedades (Id de cliente)

--- CarritoCompras/Models/Cliente.cs
+++ CarritoCompras/Models/Cliente.cs
@@ -5,7 +5,6 @@ namespace CarritoCompras.Models
 {
     public class Cliente : Usuario
     {
-        public int Id { get; set; }
         public string Dni { get; set; }
         public List<Carrito> Carritos { get; set; } = new List<Carrito>();
         public List<Compra> Compras { get; set; } = new List<Compra>();
