a48c80b8b83cbda2577b1a6d6451d75b19ef806e
Ferchamin <ferminmorello2@hotmail.com>
2021-05-18 21:56:03 -0300

    Creación de controladores y vistas de: Productos StockItems Sucursales Usuarios

---
+++ CarritoCompras/Controllers/StockItemsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarritoCompras.Data;
+using CarritoCompras.Models;
+
+namespace CarritoCompras.Controllers
+{
+    public class StockItemsController : Controller
+    {
+        private readonly MiContexto _context;
+
+        public StockItemsController(MiContexto context)
+        {
+            _context = context;
+        }
+
+        // GET: StockItems
+        public async Task<IActionResult> Index()
+        {
+            var miContexto = _context.StockItems.Include(s => s.Producto).Include(s => s.Sucursal);
+            return View(await miContexto.ToListAsync());
+        }
+
+        // GET: StockItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stockItem = await _context.StockItems
+                .Include(s => s.Producto)
+                .Include(s => s.Sucursal)
+                .FirstOrDefaultAsync(m => m.SucursalId == id);
+            if (stockItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(stockItem);
+        }
+
+        // GET: StockItems/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Descripcion");
+            ViewData["SucursalId"] = new SelectList(_context.Sucursales, "Id", "Direccion");
+            return View();
+        }
+
+        // POST: StockItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Cantidad,SucursalId,ProductoId")] StockItem stockItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(stockItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Descripcion", stockItem.ProductoId);
+            ViewData["SucursalId"] = new SelectList(_context.Sucursales, "Id", "Direccion", stockItem.SucursalId);
+            return View(stockItem);
+        }
+
+        // GET: StockItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stockItem = await _context.StockItems.FindAsync(id);
+            if (stockItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Descripcion", stockItem.ProductoId);
+            ViewData["SucursalId"] = new SelectList(_context.Sucursales, "Id", "Direccion", stockItem.SucursalId);
+            return View(stockItem);
+        }
+
+        // POST: StockItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Cantidad,SucursalId,ProductoId")] StockItem stockItem)
+        {
+            if (id != stockItem.SucursalId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(stockItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StockItemExists(stockItem.SucursalId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductoId"] = new SelectList(_context.Productos, "Id", "Descripcion", stockItem.ProductoId);
+            ViewData["SucursalId"] = new SelectList(_context.Sucursales, "Id", "Direccion", stockItem.SucursalId);
+            return View(stockItem);
+        }
+
+        // GET: StockItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var stockItem = await _context.StockItems
+                .Include(s => s.Producto)
+                .Include(s => s.Sucursal)
+                .FirstOrDefaultAsync(m => m.SucursalId == id);
+            if (stockItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(stockItem);
+        }
+
+        // POST: StockItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var stockItem = await _context.StockItems.FindAsync(id);
+            _context.StockItems.Remove(stockItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StockItemExists(int id)
+        {
+            return _context.StockItems.Any(e => e.SucursalId == id);
+        }
+    }
+}
